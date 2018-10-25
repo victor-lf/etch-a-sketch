2d768f0dd611b33b5f46d39c4ab3afd23dacd9fe
Victor <victor-lf@hotmail.com>
2018-10-25 15:07:09 +0000

    First commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# etch-a-sketch
+
+
+
+---
+
+From The Odin Project's [curriculum](https://www.theodinproject.com/courses/web-development-101/lessons/etch-a-sketch-project).
---
+++ script.js
@@ -0,0 +1,50 @@
+const containerDiv = document.getElementById('container');
+let squares = 256;
+
+function changeColor(e) {
+    const hue = Math.random() * 360;
+    const saturation = Math.random() * 100;
+    let targetLightness = e.target.getAttribute('data-lightness');
+
+    if (e.target.hasAttribute('style')) {
+        e.target.setAttribute('data-lightness', targetLightness - 5);
+        targetLightness = e.target.getAttribute('data-lightness');
+        e.target.style.backgroundColor = `hsl(${hue}, ${saturation}%, ${targetLightness}%)`;
+    } else {
+        e.target.setAttribute('data-lightness', 50);
+        e.target.style.backgroundColor = `hsl(${hue}, ${saturation}%, 50%)`;
+    }
+}
+
+function setChildren(n) {
+    while (containerDiv.firstChild) {
+        containerDiv.removeChild(containerDiv.firstChild);
+    }
+
+    for (let i = 1; i <= n; i++) {
+        containerDiv.appendChild(document.createElement('div'));
+    }
+
+    containerDiv.childNodes.forEach(div =>
+        div.addEventListener('mouseenter', changeColor));
+}
+
+function setSquares() {
+    let squaresPerSide = prompt('How many squares per side?', '');
+
+    while (isNaN(squaresPerSide) || squaresPerSide <= 0) {
+        if (squaresPerSide === null) return;
+        squaresPerSide = prompt('Invalid option. Enter a positive number.', '');
+    }
+
+    squares = Math.pow(squaresPerSide, 2);
+
+    containerDiv.style.grid = `repeat(${squaresPerSide}, auto) /
+                               repeat(${squaresPerSide}, auto)`;
+}
+
+const button = document.querySelector('button');
+button.addEventListener('click', setSquares);
+button.addEventListener('click', () => setChildren(squares));
+
+setChildren(squares);
